5a000e71ffda3a6286ff261493e2e1fb11f38eef
jankvr <jan.kvr@gmail.com>
2020-06-30 07:52:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ class Action {
 
         fs.readdirSync(".").filter(fn => /\.s?nupkg$/.test(fn)).forEach(fn => fs.unlinkSync(fn))
 
-        this._executeInProcess(`dotnet build -c Release ${this.projectFile}`)
+        this._executeInProcess(`dotnet build -c Release ${this.projectFile} ${this.version ? "-p:Version=" + this.version : ""}`)
 
         this._executeInProcess(`dotnet pack ${this.includeSymbols ? "--include-symbols -p:SymbolPackageFormat=snupkg" : ""} --no-build -c Release ${this.projectFile} -o .`)
 
